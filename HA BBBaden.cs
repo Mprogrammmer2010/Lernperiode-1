0547a8b632b0a95d8a1b460efb131e4f71580c94
Mprogrammmer2010 <m.iftikhar.im26@stud.bbbaden.ch>
2026-09-18 12:34:44 +0200

    Add files via upload

---
+++ HA BBBaden.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        double zahl = 42;
+        double tipp;
+        bool gewonnen = false;
+
+        Console.WriteLine("Zahlenratespiel");
+        Console.WriteLine("Rate die Zahl zwischen 1 und 100!");
+
+        while (gewonnen == false)
+        {
+            Console.Write("Dein Tipp: ");
+            tipp = Convert.ToDouble(Console.ReadLine());
+
+            if (tipp == zahl)
+            {
+                Console.WriteLine("Richtig! Du hast gewonnen!");
+                gewonnen = true;
+            }
+            else if (tipp < zahl)
+            {
+                Console.WriteLine("Die Zahl ist größer.");
+            }
+            else
+            {
+                Console.WriteLine("Die Zahl ist kleiner.");
+            }
+        }
+
+        Console.WriteLine("Spiel beendet!");
+        Console.ReadLine();
+    }
+}
